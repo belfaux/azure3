94b817351681107809cfd2deb3c5eb3f316de46d
Julius Sambo <juliusj.sambo@gmail.com>
2018-05-23 10:10:15 +0800

    new test!

--- app.js
+++ app.js
@@ -296,7 +296,20 @@ bot.dialog('showMCclassic', function (session) {
       .title("5% of the Amount")
       .text("Cash Advance Fee")
   ]);
-  session.send(msg).endDialog();
+  session.send(msg)
+},
+function (session, results) {
+    var msg = new builder.Message(session)
+    .text("Thank you for expressing interest in our premium golf shirt! What color of shirt would you like?")
+    .suggestedActions(
+        builder.SuggestedActions.create(
+                session, [
+                    builder.CardAction.imBack(session, "productId=1&color=green", "Green"),
+                    builder.CardAction.imBack(session, "productId=1&color=blue", "Blue"),
+                    builder.CardAction.imBack(session, "productId=1&color=red", "Red")
+                ]
+            ));
+session.send(msg);
 }
 ).triggerAction({ matches: /^MasterCard Classic/i })
 
