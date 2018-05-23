df61b262cea1caac1163ec402ed0c789973b4e33
Julius Sambo <juliusj.sambo@gmail.com>
2018-05-23 09:50:11 +0800

    POUND THE ALARM

--- app.js
+++ app.js
@@ -298,7 +298,36 @@ bot.dialog('showMCclassic', function (session) {
 }
 ).triggerAction({ matches: /^MasterCard Classic/i })
 
+bot.dialog('createAlarm', [
+    function (session) {
+        session.dialogData.alarm = {};
+        builder.Prompts.text(session, "What would you like to name this alarm?");
+    },
+    function (session, results, next) {
+        if (results.response) {
+            session.dialogData.name = results.response;
+            builder.Prompts.time(session, "What time would you like to set an alarm for?");
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        if (results.response) {
+            session.dialogData.time = builder.EntityRecognizer.resolveTime([results.response]);
+        }
 
+        // Return alarm to caller  
+        if (session.dialogData.name && session.dialogData.time) {
+            session.endDialogWithResult({ 
+                response: { name: session.dialogData.name, time: session.dialogData.time } 
+            }); 
+        } else {
+            session.endDialogWithResult({
+                resumed: builder.ResumeReason.notCompleted
+            });
+        }
+    }
+]).triggerAction({ matches: /^ALARM/i });
 
 
 bot.dialog('testprompt', function (session) {
