c1dd46dd2f6e427cf6b9a2fb40e73a05776a3548
Julius Sambo <juliusj.sambo@gmail.com>
2018-08-01 10:13:38 +0800

    test with img

--- app.js
+++ app.js
@@ -520,6 +520,26 @@ bot.dialog('inputtest', [
                        session.userData.address + ", and mobile number "
                    + session.userData.mobile + 
                     ".");
+    },
+  function (session) {
+    var msg = session.message;
+    if (msg.attachments && msg.attachments.length > 0) {
+     // Echo back attachment
+     var attachment = msg.attachments[0];
+        session.send({
+            text: "You sent:",
+            attachments: [
+                {
+                    contentType: attachment.contentType,
+                    contentUrl: attachment.contentUrl,
+                    name: attachment.name
+                }
+            ]
+        });
+    } else {
+        // Echo back users text
+        session.send("You said: %s", session.message.text);
     }
+}
     
 ]).triggerAction({ matches: /^input/i });
