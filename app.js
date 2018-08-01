ac9afe52fa242c9318509af30e06ab19493e8aa4
Julius Sambo <juliusj.sambo@gmail.com>
2018-08-01 10:57:11 +0800

    new test added to start

--- app.js
+++ app.js
@@ -49,8 +49,24 @@ var cosmosStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, do
 // Create your bot with a function to receive messages from the user
 // Create bot and default message handler
 var bot = new builder.UniversalBot(connector, function (session) {
-  //session.send("Good morning.");
-  //session.send("Hi! Welcome. Type in 'start' to begin. " );
+   var msg = session.message;
+    if (msg.attachments && msg.attachments.length > 0) {
+     // Echo back attachment
+     var attachment = msg.attachments[0];
+        session.send({
+            text: "You sent:",
+            attachments: [
+                {
+                    contentType: attachment.contentType,
+                    contentUrl: attachment.contentUrl,
+                    name: attachment.name
+                }
+            ]
+        });
+    } else {
+        // Echo back users text
+        session.send("You said: %s", session.message.text);
+    }
 }).set('storage', cosmosStorage);
 
 
@@ -520,12 +536,11 @@ bot.dialog('inputtest', [
                        session.userData.address + ", and mobile number "
                    + session.userData.mobile + 
                     ".");
-      session.send("send image")
+      session.send("send image");
     },
   function (session, results) {
     session.userData.image = results.response;
-    var msg = session.userData.image;
-    if (msg.attachments && msg.attachments.length > 0) {
+    if (session.userData.image && msg.attachments.length > 0) {
      // Echo back attachment
      var attachment = msg.attachments[0];
         session.send({
